fb95215c7492908a972597e8eb761df571da9fd0
Abhikant <awasthi.abhikant28@gmail.com>
2021-08-11 15:36:31 +0530

    Initial commit

--- app/src/main/java/com/spacECE/spaceceedu/VideoLibrary/VideoLibrary_Activity.java
+++ app/src/main/java/com/spacECE/spaceceedu/VideoLibrary/VideoLibrary_Activity.java
@@ -1,21 +1,23 @@
 package com.spacECE.spaceceedu.VideoLibrary;
 
 
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.spacECE.spaceceedu.R;
-import com.spacECE.spaceceedu.Utils.UsefulFunctions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,47 +25,88 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-import retrofit2.http.Tag;
+public class VideoLibrary_Activity extends Activity {
+    ArrayList<Topic> topicList= new ArrayList<>();
+    private RecyclerView recyclerView;
+    VideoLibrary_RecyclerViewAdapter.RecyclerViewClickListener listener;
 
-public class VideoLibrary_Activity extends AppCompatActivity {
+    private RequestQueue mQueue;
 
-    public final static boolean[] ArrayDownloadCOMPLETED = {false};
-    public static ArrayList<Topic> trendingTopicList = new ArrayList<>();//used in VideoLibrary_trending_Fragment.java
-    public static ArrayList<Topic> paidTopicList = new ArrayList<>();//used in VideoLibrary_Premium.java
-    public static ArrayList<Topic> freeTopicList = new ArrayList<>();  //used in VideoLibrary_Free.java
-    NavigationBarView.OnItemSelectedListener VL_navListener =
-            new NavigationBarView.OnItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
-                    switch (item.getItemId()) {
-                        case R.id.videolibrary_nav_free:
-                            selectedFragment = new VideoLibrary_Free();
-                            break;
-                        case R.id.videolibrary_nav_paid:
-                            selectedFragment = new VideoLibrary_Premium();
-                            break;
-                        case R.id.videolibrary_nav_trending:
-                            selectedFragment = new VideoLibrary_trending_Fragment();
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.VideoLibrary_Fragment_layout,
-                            selectedFragment).commit();
-                    return true;
-                }
-            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_library);
 
-        BottomNavigationView videoLibraryBottomNav = findViewById(R.id.VideoLibrary_Bottom_Navigation);
-        videoLibraryBottomNav.setOnItemSelectedListener(VL_navListener);
+        mQueue = Volley.newRequestQueue(this);
+        String url = "http://3.109.14.4/SpacTube/api_getAllVideos";
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.VideoLibrary_Fragment_layout,
-                    new VideoLibrary_Free()).commit();
-        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("data");
 
+                        for (int i = 0; i < jsonArray.length(); i++) {
+
+                            JSONObject response_element = new JSONObject(String.valueOf(jsonArray.getJSONObject(i)));
+
+                            Topic newTopic = new Topic( response_element.getString("v_id"),
+                                    response_element.getString("title"), response_element.getString("v_id"),
+                                    response_element.getString("filter"), response_element.getString("length"),
+                                    response_element.getString("v_url"), response_element.getString("v_date"),
+                                            response_element.getString("v_uni_no"),response_element.getString("desc"));
+
+                            topicList.add(newTopic);
+                        }
+                        setAdapter();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Log.i("    API","NO RESPONSE !!!!!!!!!!!!!!!!!");
+            }
+        });
+
+        recyclerView= findViewById(R.id.RecycleView);
+
+        mQueue.add(request);
+
+
+
+    }
+
+     void setList() {
+       //
+    }
+
+    private void setAdapter() {
+        Log.i("SetAdapter:","Working");
+        setOnClickListener();
+        VideoLibrary_RecyclerViewAdapter adapter = new VideoLibrary_RecyclerViewAdapter(topicList,listener);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        Log.i("Adapter", "Executed");
+    }
+
+    private void setOnClickListener() {
+        listener = new VideoLibrary_RecyclerViewAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+
+                Intent intent = new Intent(getApplicationContext(), TopicActivity.class);
+                intent.putExtra("topic_name", topicList.get(position).getTitle());
+                intent.putExtra("v_url", topicList.get(position).getV_URL());
+                intent.putExtra("discrp", topicList.get(position).getDesc());
+                intent.putExtra("status",topicList.get(position).getStatus());
+
+                startActivity(intent);
+            }
+        };
     }
 }
