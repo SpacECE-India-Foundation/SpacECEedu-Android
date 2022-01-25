11be08670bdb1cbb7c74347f95e5b079abbef617
lord <ramashisx@gmail.com>
2022-01-25 10:32:40 +0530

    avinash

--- app/src/main/java/com/spacECE/spaceceedu/LibForSmall/library_RecycleAdapter.java
+++ app/src/main/java/com/spacECE/spaceceedu/LibForSmall/library_RecycleAdapter.java
@@ -1,11 +1,8 @@
 package com.spacECE.spaceceedu.LibForSmall;
 
-import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,71 +10,40 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.spacECE.spaceceedu.LibForSmall.books;
 import com.spacECE.spaceceedu.R;
-import com.spacECE.spaceceedu.Utils.ConfigUtils;
-import com.squareup.picasso.Picasso;
-
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class library_RecycleAdapter extends RecyclerView.Adapter<library_RecycleAdapter.MyViewHolder>{
 
     ArrayList<books> list;
-    private Context context;
+    private final library_RecycleAdapter.RecyclerViewClickListener listener;
 
-    private final RecyclerViewClickListener listener;
-
-    public library_RecycleAdapter(ArrayList<books> myList, RecyclerViewClickListener listener,Context context) {
+    public library_RecycleAdapter(ArrayList<books> myList, RecyclerViewClickListener listener) {
         this.list = myList;
         this.listener = listener;
-        this.context = context;
     }
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public library_RecycleAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.library_list_listitem, parent, false);
-        return new MyViewHolder(itemView);
+        return new library_RecycleAdapter.MyViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        try {
-            JSONObject config = ConfigUtils.loadConfig(context);
-            if (config != null) {
-                String baseUrl= config.getString("BASE_URL");
-                String libProductimgUrl = config.getString("LIB_PRODUCTIMG");
-
-                holder.book_name.setText(list.get(position).getProduct_title());
-                holder.book_category.setText(list.get(position).getProduct_desc());
-                holder.book_price.setText(list.get(position).getProduct_price());
-                Picasso.get()
-                        .load(baseUrl+libProductimgUrl+list.get(position).product_image)
-                        .error(R.drawable.tile_icon_2)
-                        .into(holder.book_image);
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            Log.i("ERROR:::", "Failed to load API URLs");
-        }
+        holder.book_name.setText(list.get(position).getProduct_title());
+        holder.book_category.setText(list.get(position).getProduct_desc());
+        holder.book_price.setText(list.get(position).getExchange_price());
     }
-
 
     @Override
     public int getItemCount() {
         return list.size();
     }
 
-    public void setData(ArrayList<books> newList) {
-        this.list = newList;
-        notifyDataSetChanged();
-    }
-
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
         private final TextView book_name;
-        private final ImageView book_image;
         private final TextView book_category ;
         private final TextView book_price;
         public MyViewHolder(@NonNull View view) {
@@ -87,8 +53,6 @@ public class library_RecycleAdapter extends RecyclerView.Adapter<library_Recycle
             book_price=view.findViewById(R.id.cardview_price);
             view.setOnClickListener(this);
             book_category=view.findViewById(R.id.cardview_category);
-            view.setOnClickListener(this);
-            book_image=view.findViewById(R.id.cardview_bookimage);
             view.setOnClickListener(this);
         }
 
