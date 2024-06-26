c1e4ab0299f794e20b834fa4023c4d0d4e9b5689
ASHISH SAHU <supershor.cp@gmail.com>
2024-06-26 12:51:33 +0530

    Second page intents added

---
+++ app/src/main/java/com/spacECE/spaceceedu/space_active/second_page_for_space_active_with_image.java
@@ -0,0 +1,26 @@
+package com.spacECE.spaceceedu.space_active;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.spacECE.spaceceedu.R;
+
+public class second_page_for_space_active_with_image extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second_page_for_space_active_with_image);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
