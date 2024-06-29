dd3a8321a47a589a1718785455ffd3a1e2efad9e
ASHISH SAHU <supershor.cp@gmail.com>
2024-06-29 15:27:37 +0530

    almost done 2

--- app/src/main/java/com/spacECE/spaceceedu/space_active/ActivitiesListActivity.java
+++ app/src/main/java/com/spacECE/spaceceedu/space_active/ActivitiesListActivity.java
@@ -3,9 +3,14 @@ package com.spacECE.spaceceedu.space_active;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -34,57 +39,156 @@ public class ActivitiesListActivity extends AppCompatActivity implements ClickLi
     HashMap<String,ArrayList<space_active_data_holder_all_in_one>>domain=new HashMap<>();
     HashMap<String,ArrayList<space_active_data_holder_all_in_one>>key_domain=new HashMap<>();
     HashMap<String,String>activity_completed=new HashMap<>();
+
+    AppCompatButton button_free;
+    AppCompatButton button_paid;
+    AppCompatButton all_button;
+    ArrayList<String>key_domain_list;
+    Spinner key_spinner;
+
+    ArrayList<String>dev_domain_list;
+    Spinner dev_spinner;
+
+
+
+    ArrayList<String>level_list;
+    Spinner level_spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activities_list);
 
+
         getWindow().setStatusBarColor(ContextCompat.getColor(ActivitiesListActivity.this,R.color.black));
+        key_spinner = findViewById(R.id.key_domain_spinner);dev_spinner = findViewById(R.id.dev_domain_spinner);
         list_activity=findViewById(R.id.list_activity);
+        level_spinner=findViewById(R.id.level_spinner);
         fetch();
+
+        button_free=findViewById(R.id.button_free);
+        button_paid=findViewById(R.id.button_paid);
+        all_button=findViewById(R.id.all_button);
+
+        button_free.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActivitiesListActivity.this,free_arrayList_space_active_all_in_one_data_holder.size()+"", Toast.LENGTH_SHORT).show();
+                set_free_activities();
+                Toast.makeText(ActivitiesListActivity.this, " Refresh Done for free ", Toast.LENGTH_SHORT).show();
+            }
+        });all_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                set_all_activities();
+                Toast.makeText(ActivitiesListActivity.this, " Refresh Done for all ", Toast.LENGTH_SHORT).show();
+            }
+        });
+        button_paid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActivitiesListActivity.this,paid_arrayList_space_active_all_in_one_data_holder.size()+"", Toast.LENGTH_SHORT).show();
+                set_paid_activities();
+                Toast.makeText(ActivitiesListActivity.this, " Refresh Done for paid ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        key_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                set_key_domain_activities(key_domain_list.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+
+        //added for testing
+
+
+        dev_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                set_domain_activities(dev_domain_list.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+        level_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                set_level_activities(level_list.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+
+
+
     }
     public void set_level_activities(String string) {
-        ArrayList<space_active_data_holder_all_in_one> local=level.get(string);
-        Log.e( "onResponse:1!!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne=new RecyclerView_Adapter_activities_all_in_one(local,ActivitiesListActivity.this,ActivitiesListActivity.this,activity_completed);
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(ActivitiesListActivity.this);
-        list_activity.setLayoutManager(layoutManager);
-        list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
-        Log.e( "onResponse:2!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (string.equals("All")){
+            set_all_activities();
+        }else {
+            ArrayList<space_active_data_holder_all_in_one> local = level.get(string);
+            RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne = new RecyclerView_Adapter_activities_all_in_one(local, ActivitiesListActivity.this, ActivitiesListActivity.this, activity_completed);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ActivitiesListActivity.this);
+            list_activity.setLayoutManager(layoutManager);
+            list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
+        }
     }
     public void set_domain_activities(String string) {
-        ArrayList<space_active_data_holder_all_in_one> local=domain.get(string);
-        Log.e( "onResponse:1!!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne=new RecyclerView_Adapter_activities_all_in_one(local,ActivitiesListActivity.this,ActivitiesListActivity.this,activity_completed);
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(ActivitiesListActivity.this);
-        list_activity.setLayoutManager(layoutManager);
-        list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
-        Log.e( "onResponse:2!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (string.equals("All")){
+            set_all_activities();
+        }else {
+            ArrayList<space_active_data_holder_all_in_one> local = domain.get(string);
+            RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne = new RecyclerView_Adapter_activities_all_in_one(local, ActivitiesListActivity.this, ActivitiesListActivity.this, activity_completed);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ActivitiesListActivity.this);
+            list_activity.setLayoutManager(layoutManager);
+            list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
+        }
     }
     public void set_key_domain_activities(String string) {
-        ArrayList<space_active_data_holder_all_in_one> local=key_domain.get(string);
-        Log.e( "onResponse:1!!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne=new RecyclerView_Adapter_activities_all_in_one(local,ActivitiesListActivity.this,ActivitiesListActivity.this,activity_completed);
-        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(ActivitiesListActivity.this);
-        list_activity.setLayoutManager(layoutManager);
-        list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
-        Log.e( "onResponse:2!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (string.equals("All")){
+            set_all_activities();
+        }else {
+            ArrayList<space_active_data_holder_all_in_one> local=key_domain.get(string);
+            RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne=new RecyclerView_Adapter_activities_all_in_one(local,ActivitiesListActivity.this,ActivitiesListActivity.this,activity_completed);
+            RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(ActivitiesListActivity.this);
+            list_activity.setLayoutManager(layoutManager);
+            list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
+        }
     }
     public void set_free_activities() {
-        Log.e( "onResponse:1!!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne=new RecyclerView_Adapter_activities_all_in_one(free_arrayList_space_active_all_in_one_data_holder,ActivitiesListActivity.this,ActivitiesListActivity.this,activity_completed);
         RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(ActivitiesListActivity.this);
         list_activity.setLayoutManager(layoutManager);
         list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
-        Log.e( "onResponse:2!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+    public void set_all_activities() {
+        RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne=new RecyclerView_Adapter_activities_all_in_one(arrayList_space_active_all_in_one_data_holder,ActivitiesListActivity.this,ActivitiesListActivity.this,activity_completed);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(ActivitiesListActivity.this);
+        list_activity.setLayoutManager(layoutManager);
+        list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
     }
     public void set_paid_activities() {
-        Log.e( "onResponse:1!!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne=new RecyclerView_Adapter_activities_all_in_one(paid_arrayList_space_active_all_in_one_data_holder,ActivitiesListActivity.this,ActivitiesListActivity.this,activity_completed);
         RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(ActivitiesListActivity.this);
         list_activity.setLayoutManager(layoutManager);
         list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
-        Log.e( "onResponse:2!!!!!!!!!!!!!!","!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
     @Override
     protected void onRestart() {
@@ -99,25 +203,25 @@ public class ActivitiesListActivity extends AppCompatActivity implements ClickLi
     }
 
     public void fetch(){
-        arrayList_space_active_all_in_one_data_holder.clear();
-        free_arrayList_space_active_all_in_one_data_holder.clear();
-        paid_arrayList_space_active_all_in_one_data_holder.clear();
-        level.clear();
-        domain.clear();
-        key_domain.clear();
-        activity_completed.clear();
         RequestQueue requestQueue= Volley.newRequestQueue(ActivitiesListActivity.this);
         String url="http://43.205.45.96/api/spaceactive_activities.php";
         JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                Log.e( "onResponse:-----------------",response.toString());
+                arrayList_space_active_all_in_one_data_holder.clear();
+                free_arrayList_space_active_all_in_one_data_holder.clear();
+                paid_arrayList_space_active_all_in_one_data_holder.clear();
+                level.clear();
+                domain.clear();
+                key_domain_list=new ArrayList<>();
+                level_list=new ArrayList<>();
+                dev_domain_list=new ArrayList<>();
+                key_domain.clear();
                 JSONArray jsonArray= null;
                 try {
                     jsonArray = response.getJSONArray("data");
                     for(int i=0;i<jsonArray.length();i++) {
                         JSONObject jsonObject=jsonArray.getJSONObject(i);
-                        Log.e( "onResponse:----"+i+"------",jsonObject.toString());
                         String activity_no=jsonObject.getString("activity_no");
                         String activity_name=jsonObject.getString("activity_name");
                         String activity_level=jsonObject.getString("activity_level");
@@ -146,7 +250,6 @@ public class ActivitiesListActivity extends AppCompatActivity implements ClickLi
                         if (jsonObject.getString("work_done")!=null){
                             arrayList_space_active_all_in_one_data_holder.get(i).setActivity_complete_status(jsonObject.getString("work_done"));
                         }
-                        Log.e( "onResponse:-------------------------------------------------","----------------------------------------------------------");
                         arrayList_space_active_all_in_one_data_holder.get(i).print_All();
                         if (arrayList_space_active_all_in_one_data_holder.get(i).activity_type_status.equals("free")){
                             free_arrayList_space_active_all_in_one_data_holder.add(arrayList_space_active_all_in_one_data_holder.get(i));
@@ -156,13 +259,13 @@ public class ActivitiesListActivity extends AppCompatActivity implements ClickLi
                             Log.e( "paid:>>>>>>>>>>>>>>>>>>>>",arrayList_space_active_all_in_one_data_holder.get(i).activity_no);
                         }
                         if (level.containsKey(arrayList_space_active_all_in_one_data_holder.get(i).activity_level)){
-                            Log.e( "Old okok :>>>>>>>>>>>>>>>>>",arrayList_space_active_all_in_one_data_holder.get(i).activity_level);
+                            Log.e( "Old okok :!!!!!!!!!!!!!!!!!!!!!!!!!",arrayList_space_active_all_in_one_data_holder.get(i).activity_level);
                             level.get(arrayList_space_active_all_in_one_data_holder.get(i).activity_level).add(arrayList_space_active_all_in_one_data_holder.get(i));
                         }else {
                             ArrayList<space_active_data_holder_all_in_one>arrayList=new ArrayList<>();
                             arrayList.add(arrayList_space_active_all_in_one_data_holder.get(i));
                             level.put(arrayList_space_active_all_in_one_data_holder.get(i).activity_level,arrayList);
-                            Log.e( "New okokokok :>>>>>>>>>>>>>>>>>",arrayList_space_active_all_in_one_data_holder.get(i).activity_level);
+                            Log.e( "New okokokok :!!!!!!!!!!!!!!",arrayList_space_active_all_in_one_data_holder.get(i).activity_level);
                         }
 
                         if (domain.containsKey(arrayList_space_active_all_in_one_data_holder.get(i).activity_dev_domain)){
@@ -185,12 +288,27 @@ public class ActivitiesListActivity extends AppCompatActivity implements ClickLi
                             key_domain.put(arrayList_space_active_all_in_one_data_holder.get(i).activity_key_dev,arrayList);
                         }
                     }
-                    Log.e( "onResponse:1","---------------------------------------");
                     RecyclerView_Adapter_activities_all_in_one recyclerViewAdapterActivitiesAllInOne=new RecyclerView_Adapter_activities_all_in_one(arrayList_space_active_all_in_one_data_holder,ActivitiesListActivity.this,ActivitiesListActivity.this,activity_completed);
                     RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(ActivitiesListActivity.this);
                     list_activity.setLayoutManager(layoutManager);
                     list_activity.setAdapter(recyclerViewAdapterActivitiesAllInOne);
-                    Log.e( "onResponse:2","---------------------------------------");
+                    key_domain_list=new ArrayList<>(key_domain.keySet());
+                    key_domain_list.add(0,"All");
+                    ArrayAdapter<String>arrayAdapter=new ArrayAdapter<>(ActivitiesListActivity.this,R.layout.support_simple_spinner_dropdown_item,key_domain_list);
+                    key_spinner.setAdapter(arrayAdapter);
+
+
+                    dev_domain_list=new ArrayList<>(domain.keySet());
+                    dev_domain_list.add(0,"All");
+                    ArrayAdapter<String>dev_domain_adapter=new ArrayAdapter<>(ActivitiesListActivity.this,R.layout.support_simple_spinner_dropdown_item,dev_domain_list);
+                    dev_spinner.setAdapter(dev_domain_adapter);
+
+
+
+                    level_list=new ArrayList<>(level.keySet());
+                    level_list.add(0,"All");
+                    ArrayAdapter<String>level_adapter=new ArrayAdapter<>(ActivitiesListActivity.this,R.layout.support_simple_spinner_dropdown_item,level_list);
+                    level_spinner.setAdapter(level_adapter);
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
@@ -203,14 +321,12 @@ public class ActivitiesListActivity extends AppCompatActivity implements ClickLi
             }
         });
         requestQueue.add(jsonObjectRequest);
-
         String url1="http://43.205.45.96/spacec_active/api_fetchWorkdone.php?user_id=";
         if (MainActivity.ACCOUNT !=null && MainActivity.ACCOUNT.getAccount_id()!=null){
-            Log.e("fetch:!@#!@#$%^&*(@#$%^&*()","user_id================"+MainActivity.ACCOUNT.getAccount_id());
             JsonObjectRequest jsonObjectRequest1=new JsonObjectRequest(url1 + MainActivity.ACCOUNT.getAccount_id(), new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
-                    Log.e( "onResponse:@@@@@@@@@@@@@@",response.toString());
+                    activity_completed.clear();
                     try {
                         JSONArray jsonArray=response.getJSONArray("activities");
                         for (int i=0;i<jsonArray.length();i++){
