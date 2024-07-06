6a4c414fa3df72d9158a9381c4ddd4b7abcfdbe4
Supreet Kumar Nayak <gaurangadas1728@gmail.com>
2024-07-06 08:04:22 +0530

    Fragment dal diya hai

--- app/src/main/java/com/spacECE/spaceceedu/Utils/UsefulFunctions.java
+++ app/src/main/java/com/spacECE/spaceceedu/Utils/UsefulFunctions.java
@@ -2,79 +2,111 @@ package com.spacECE.spaceceedu.Utils;
 
 import android.util.Log;
 
-import okhttp3.*;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UsefulFunctions {
 
-  public static JSONObject UsingGetAPI(String inputURL) {
+    public static JSONObject UsingGetAPI(String url) {
 
-      System.out.println(inputURL);
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
 
-      JSONObject jsonObject = null;
+        try {
+            URL requestUrl = new URL(url);
+            urlConnection = (HttpURLConnection) requestUrl.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
 
-      Response response;
-      String resp;
+            int responseCode = urlConnection.getResponseCode();
+            Log.d("API Response Code", String.valueOf(responseCode));
 
-      OkHttpClient client = new OkHttpClient();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                Log.e("API Error", "Response Code: " + responseCode);
+                return null;
+            }
 
-      Request request = new Request.Builder().url(inputURL).build();
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuilder buffer = new StringBuilder();
+            if (inputStream == null) {
+                Log.e("API Error", "InputStream is null");
+                return null;
+            }
 
-      Call call = client.newCall(request);
+            reader = new BufferedReader(new InputStreamReader(inputStream));
 
-      try {
-          response = call.execute();
-          resp = response.body().string();
-          System.out.println(resp);
-          jsonObject= new JSONObject(resp);
-      } catch (IOException | JSONException e) {
-          e.printStackTrace();
-      }
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line).append("\n");
+            }
 
-      return jsonObject;
+            if (buffer.length() == 0) {
+                Log.e("API Error", "Buffer is empty");
+                return null;
+            }
 
-  }
+            String jsonStr = buffer.toString();
+            Log.d("API JSON Response", jsonStr);
+            return new JSONObject(jsonStr);
+        } catch (IOException | JSONException e) {
+            Log.e("API Error", e.getMessage(), e);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Log.e("BufferedReader Error", e.getMessage(), e);
+                }
+            }
+        }
+    }
 
-  public static class DateFunc {
+    public static class DateFunc {
 
-      public static Date StringToDate(String date) throws ParseException {
-          Log.e( "StringToDate: ", date);
-          if (date.length()==10){
-              if (date.contains(":")){
-                  date=date.replace(":","-");
-                  return new SimpleDateFormat("yyyy-MM-dd").parse(date);
-              }else {
-                  return new SimpleDateFormat("yyyy-MM-dd").parse(date);
-              }
-          }
-          else {
-              return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date);
-          }
-      }
+        public static Date StringToDate(String date) throws ParseException {
+            Log.e("StringToDate: ", date);
+            if (date.length() == 10) {
+                if (date.contains(":")) {
+                    date = date.replace(":", "-");
+                    return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+                } else {
+                    return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+                }
+            } else {
+                return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(date);
+            }
+        }
 
-      public static Date StringToTime(String date) throws ParseException {
-          Log.e( "StringToTime: ",date.toString());
-          return new SimpleDateFormat("HH:mm:ss").parse(date);
-      }
+        public static Date StringToTime(String date) throws ParseException {
+            Log.e("StringToTime: ", date.toString());
+            return new SimpleDateFormat("HH:mm:ss").parse(date);
+        }
 
-      public static String DateObjectToDate(Date date){
-          return new SimpleDateFormat("MMM/dd").format(date);
-      }
+        public static String DateObjectToDate(Date date) {
+            return new SimpleDateFormat("MMM/dd").format(date);
+        }
 
-      public static String DateObjectToTime(Date date){
-          Log.e( "DateObjectToTime:--------",date+"");
-          return new SimpleDateFormat("HH:mm").format(date);
-      }
+        public static String DateObjectToTime(Date date) {
+            Log.e("DateObjectToTime:--------", date + "");
+            return new SimpleDateFormat("HH:mm").format(date);
+        }
 
-      public static String DateObjectToDay(Date date){
-          return new SimpleDateFormat("EEEE").format(date);
-      }
-  }
-
+        public static String DateObjectToDay(Date date) {
+            return new SimpleDateFormat("EEEE").format(date);
+        }
+    }
 }
