30b3d2da373d1051a62635eff4837efd73beb695
Mudit Choudhary <mudit292005@gmail.com>
2024-07-22 13:55:35 +0530

    Project info updated

---
+++ app/src/main/java/com/spacECE/spaceceedu/VideoLibrary/About_SpaceTube.java
@@ -0,0 +1,10 @@
+package com.spacECE.spaceceedu.VideoLibrary;
+
+public class About_SpaceTube {
+//    Updated By: Mudit Choudhary
+//    GitHub:- (https://github.com/Mudit-pip)
+//    Linkedin:- (https://www.linkedin.com/in/mudit-choudhary-05142b1b4/)
+//    SpaceTube is Complete as of now but for further up gradation someone
+//    may add a recommended videos feature.
+//    It would require a new API(currently Not Available).
+}
